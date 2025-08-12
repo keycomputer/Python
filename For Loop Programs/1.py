7363dc0e87da3a6c5aa35df698637f2b38de1679
Key Computer <keycomputereducation@gmail.com>
2025-08-12 11:35:25 +0530

    stats

---
+++ For Loop Programs/1.py
@@ -0,0 +1,42 @@
+# // looping
+
+# range 
+#####
+# (start, stop , step ) 
+# (1,11,1) -> start 1, stop 10, step 1 
+# (1,11,2) -> start 1, stop 10, step 2 
+# for i in range(1,11,2):
+#     print("hello", i )
+
+# for x in range(10,0,-1): 
+#     print(x)
+
+### 
+# (start, stop ) -> default step 1 
+
+# for i in range(5,15):
+#     print(i)
+
+### 
+# (stop )  -> start 0, step 1 
+# for i in range(11):
+#     print(i)
+
+#########################
+
+for i in range(1, 15, 3):
+    print(i, end=" ")
+    
+print()
+
+for i in range(1, 30, 4):
+    print(i, end=" ")
+    
+print()
+
+for i in range(11, 6, -1):
+    print(i, end=" ")
+
+
+    
+    
---
+++ For Loop Programs/2.py
@@ -0,0 +1,62 @@
+# ////////////  print Programs ////////////////////#
+
+## print n natural numbers  
+# num = int(input("enter n "))
+
+# for i in range(1 , num+1):
+#     print(i, end="\t")
+
+
+## print range 
+
+# num1 = int(input("enter start value "))
+# num2 = int(input("enter end value "))
+
+# for i in range(num1, num2+1):
+#     print(i, end="\t")
+
+## print range 
+
+# num1 = int(input("enter start value "))
+# num2 = int(input("enter end value "))
+
+# if num1 < num2 :
+#     for i in range(num1, num2+1):
+#         print(i, end="\t")
+# else:
+#     for i in range(num1, num2-1, -1):
+#         print(i, end="\t")
+###################################################
+
+# Odd or even print
+# num = int(input("enter n "))
+
+# for i in range(1 , num+1):
+#     if i % 2 == 0:
+#         print(i, "Even Number ")
+#     else:
+#         print(i, "Odd Number ")
+# print("Only Even ")
+# for i in range(1 , num+1):
+#     if i % 2 == 0:
+#         print(i, end="\t")
+# print("\nOnly Odd ")
+# for i in range(1 , num+1):
+#     if i % 2 == 1:
+#         print(i, end="\t")
+
+###########  Divisibility
+
+# num1 = int(input("Enter start "))
+# num2 = int(input("Enter stop "))
+
+# n = int(input("Enter number "))
+# for i in range(num1, num2+1):
+#     if i % n == 0 :
+#         print(i, end=" ")
+
+############# multiplication Table 
+# num  = int(input("Enter number "))
+# for i in range(1, 11):
+#     print(num ,"*", i ,"=", num * i )
+
